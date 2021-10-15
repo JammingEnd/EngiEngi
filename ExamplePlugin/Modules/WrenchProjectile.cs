d8b25906bfc626ac5f31b302b7c5a7585ca0defc
JammingEnd <jaimeyregeling@gmail.com>
2021-10-15 10:36:58 +0200

    hoi

--- ExamplePlugin/Modules/WrenchProjectile.cs
+++ ExamplePlugin/Modules/WrenchProjectile.cs
@@ -1,16 +1,14 @@
 ﻿using R2API;
 using RoR2;
+
 using RoR2.Projectile;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace EngiEgni.Modules
 {
     internal static class WrenchProjectile
     {
         internal static GameObject wrenchPrefab;
-
-
 
         internal static void CreateProjectile()
         {
@@ -21,36 +19,22 @@ namespace EngiEgni.Modules
         {
             wrenchPrefab = ClonePrefab("CommandoGrenadeProjectile", "EngineerWrench"); //add prefab
 
-            ProjectileSingleTargetImpact wrenchImpact = wrenchPrefab.GetComponent<ProjectileSingleTargetImpact>();
-          
-           
-            StartImpactEvent(wrenchImpact);
-          
+            On.RoR2.Projectile.ProjectileImpactEventCaller.OnProjectileImpact += ProjectileImpactEventCaller_OnProjectileImpact;
         }
-        private static void AddBATComponentOnAddDeployableHook(On.RoR2.CharacterMaster.orig_AddDeployable orig, CharacterMaster self, Deployable deployable, DeployableSlot slot)
+
+        private static void ProjectileImpactEventCaller_OnProjectileImpact(On.RoR2.Projectile.ProjectileImpactEventCaller.orig_OnProjectileImpact orig, ProjectileImpactEventCaller self, ProjectileImpactInfo whatIHit)
         {
-            orig(self, deployable, slot);
+            orig(self, whatIHit);
+            ProjectileSingleTargetImpact projectileImpactEvent = self.GetComponent<ProjectileSingleTargetImpact>();
 
-            if (slot == DeployableSlot.EngiTurret)
-            {
-               var badAssTurret = deployable.gameObject.AddComponent<UpgradeTurretComp>();
-            }
-        }
-        
-
-
-        private static void StartImpactEvent(ProjectileSingleTargetImpact projectileImpactEvent)
-        {
             float projDamage = 15;
-           
-            ProjectileImpactInfo whatIHit = new ProjectileImpactInfo();
+
             if (whatIHit.collider.gameObject.GetComponent<UpgradeTurretComp>() != null)
             {
                 UpgradeTurretComp addStack = whatIHit.collider.gameObject.GetComponent<UpgradeTurretComp>();
                 addStack.addBuffAndUpdateInt();
             }
-            
-         
+
             if (whatIHit.collider.GetComponent<HealthComponent>().shield > 0)
             {
                 projDamage = 0.75f;
@@ -58,20 +42,32 @@ namespace EngiEgni.Modules
             else
             {
                 projDamage = 0.5f;
-                
             }
-            
 
             projectileImpactEvent.projectileDamage.damage = projDamage;
             projectileImpactEvent.projectileDamage.crit = false;
             projectileImpactEvent.projectileDamage.force = 2;
             projectileImpactEvent.projectileDamage.damageType = DamageType.Generic;
         }
+
+        private static void AddBATComponentOnAddDeployableHook(On.RoR2.CharacterMaster.orig_AddDeployable orig, CharacterMaster self, Deployable deployable, DeployableSlot slot)
+        {
+            orig(self, deployable, slot);
+
+            if (slot == DeployableSlot.EngiTurret)
+            {
+                var badAssTurret = deployable.gameObject.AddComponent<UpgradeTurretComp>();
+            }
+        }
+
+        private static void StartImpactEvent()
+        {
+        }
+
         private static GameObject ClonePrefab(string prefabName, string newPrefabName)
         {
             GameObject newPrefab = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("Prefabs/Projectiles/" + prefabName), newPrefabName);
             return newPrefab;
-        } 
-
+        }
     }
 }
